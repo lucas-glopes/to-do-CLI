ecee3d112deeae11a0e0fb7140b611e4f2cd7d46
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-16 18:11:14 -0300

    Criação da função cabecalho().
    A função recebe um texto (obrigatório) e um tamanho (opcional) e imprime
    um cabeçalho no formato personalizado do programa.

--- utilitarios.py
+++ utilitarios.py
@@ -3,3 +3,10 @@ def titulo(texto: str, tamanho: int = 50) -> None:
     print("-" * tamanho)
     print(texto.center(tamanho))
     print("-" * tamanho)
+
+
+def cabecalho(texto: str, tamanho: int = 50) -> None:
+    """Imprime um cabeçalho em formato personalizado."""
+    print("=" * tamanho)
+    print(texto.upper().center(tamanho))
+    print("=" * tamanho)
