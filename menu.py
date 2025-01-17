7d4b381b47ca2e1187265da2f3a347dfd21c6750
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-16 21:45:24 -0300

    Criação da função imprimir().
    A função imprime o menu principal. Ela recebe a lista de opções
    (obrigatória) e o tamanho (opcional) e imprime o cabeçalho para o menu e
    as opções no formato personalizado do programa.
    
    A função cabecalho() - do módulo utilitarios.py - foi importada a fim de
    imprimir o cabeçalho do menu.

--- menu.py
+++ menu.py
@@ -1,3 +1,15 @@
+from utilitarios import cabecalho
+
+
+def imprimir(opcoes: list, tamanho=50) -> None:
+    """Imprime o menu principal em formato personalizado."""
+    cabecalho(texto="menu", tamanho=tamanho)
+    print("-" * tamanho)
+    for indice, opcao in enumerate(opcoes):
+        print(f"[ {indice + 1} ]" + opcao.capitalize().center(tamanho - 10))
+        print("-" * tamanho)
+
+
 def main() -> None: ...
 
 
