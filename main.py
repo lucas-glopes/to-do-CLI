b1bedb78a0fe0fc92e8f93fb70510cdffa2768a6
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-18 11:58:25 -0300

    Criação da função executar().
    Essa função será responsável por chamar outras funções para executar a
    tarefa da opção escolhida pelo usuário.
    
    A função recebe como argumentos a opção escolhida pelo usuário, o nome
    do arquivo da lista de tarefas e o tamanho - largura, em caracteres -
    máximo que as seções terão.
    
    Foi usada uma estrutura match case para direcionar o usuário a cada uma
    das oções possíveis (1, 2 e 3 - excetuando a opção de sair, que será
    executada pelo função main()), além de um case padrão (case _) para
    casos imprevistos.
    
    Foi adicionada uma ellipsis (...) no espaço reservado para cada caso, o
    qual será preenchido conforme os módulos necessários forem
    desenvolvidos.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import menu
 
 
+def executar(opcao_escolhida: int, nome_arquivo: str, tamanho: int):
+    match opcao_escolhida:
+        case 1:
+            ...
+        case 2:
+            ...
+        case 3:
+            ...
+        case _:
+            ...
+
+
 def main() -> None:
     OPCOES: list = ["adicionar tarefas", "visualizar lista", "editar tarefas", "sair"]
     TAMANHO: int = 50
