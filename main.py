7a3c527cc6d29c535a06395294747e7281381089
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-18 10:22:52 -0300

    Implementação do menu no módulo principal (main.py).
    O módulo menu.py foi importado e as funções imprimir() e
    selecionar_opcao() desse módulo foram utilizadas para, respectivamente,
    imprimir o menu de opções ao usuário e armazenar a opção escolhida por
    este em uma variável que será utilizada posteriormente.
    
    As constantes OPCOES (uma lista com as opções disponíveis no menu) e
    TAMANHO (para definir a largura - em carateres - do menu) foram criadas
    a fim de serem usadas como argumento tanto para as funções do menu
    quanto para funções que ainda serão implementadas.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def main() -> None: ...
+import menu
+
+
+def main() -> None:
+    OPCOES: list = ["adicionar tarefas", "visualizar lista", "editar tarefas", "sair"]
+    TAMANHO: int = 50
+
+    menu.imprimir(opcoes=OPCOES, tamanho=TAMANHO)
+    opcao_escolhida: int = menu.selecionar_opcao(opcoes=OPCOES)
 
 
 if __name__ == "__main__":
