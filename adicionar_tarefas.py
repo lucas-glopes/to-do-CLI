7bdc65f1e06901a493f159542e85a3ddb8fabfb0
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-19 16:26:25 -0300

    Criação das funções adicionar() e criar_lista().
    A função adicionar() recebe o nome do arquivo da lista de tarefas
    (opcional) e um tamanho (opcional) como argumentos, cria o arquivo caso
    ele ainda não exista no diretório atual, imprime o cabeçalho para a
    seção de adicionar tarefas e entra num loop while que permite ao usuário
    informar título e descrição para uma nova tarefa. Após informados título
    e descrição, é perguntado ao usuário se ele deseja continuar adicionando
    tarefas. Em caso negativo, o loop é finalizado e as novas tarefas são
    registradas na lista de tarefas.
    
    A função criar_lista() recebe um nome do arquivo (opcional) para servir
    como lista e cria esse arquivo (por padrão, no formato CSV), já
    adicionando o nome dos campos (título, descrição e status) neste.

--- adicionar_tarefas.py
+++ adicionar_tarefas.py
@@ -14,5 +14,54 @@ from utilitarios import (
 import csv
 
 
+def adicionar(nome_arquivo: str = "tarefas.csv", tamanho: int = 50) -> None:
+    """Recebe título e descrição das tarefas em loop e as registra na lista de tarefas."""
+    if not Path.exists(Path.cwd() / nome_arquivo):
+        criar_lista(nome_arquivo, tamanho)
+
+    cabecalho(texto="adicionar tarefas", tamanho=tamanho)
+    novas_tarefas: list[dict[str, str]] = []
+
+    while True:
+        titulo(texto="Nova tarefa", tamanho=tamanho)
+        while True:
+            titulo_tarefa: str = entrada("Título:")
+            if len(titulo_tarefa) > 0:
+                break
+            erro("o título da tarefa é obrigatório!")
+        descricao_tarefa: str = entrada("Descrição:")
+        novas_tarefas.append(
+            {
+                "Título": titulo_tarefa,
+                "Descrição": descricao_tarefa,
+                "Status": "pendente",
+            }
+        )
+
+        while True:
+            continuar: str = entrada("Adicionar mais tarefas? [s/n]").lower().strip()
+            if resposta_valida(continuar):
+                break
+            erro("resposta inválida!")
+
+        if not continuar in ["sim", "s"]:
+            break
+
+    registrar_tarefas(tarefas=novas_tarefas, nome_arquivo=nome_arquivo)
+    informacao(mensagem="novas tarefas registradas", tamanho=tamanho)
+
+
+def criar_lista(nome_arquivo: str = "tarefas.csv", tamanho=50) -> None:
+    """Cria/sobrescreve o arquivo de lista com o nome fornecido."""
+    with open(nome_arquivo, "w") as arquivo_tarefas:
+        CAMPOS: list[str] = ["Título", "Descrição", "Status"]
+        escritor_csv: object = csv.DictWriter(f=arquivo_tarefas, fieldnames=CAMPOS)
+        escritor_csv.writeheader()
+        arquivo_tarefas.close()
+
+    informacao(mensagem=f'arquivo "{nome_arquivo}" criado', tamanho=tamanho)
+
+
+def main() -> None:
 if __name__ == "__main__":
     main()
