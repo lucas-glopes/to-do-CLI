b5f39592cf55ae80a0bbe8f2878adb4607b31df9
Lucas Gomes Lopes <lucas.glopes3.0@gmail.com>
2025-01-25 09:03:43 -0300

    Criação das funções editar() e executar().
    A função editar() recebe a lista de ações (obrigatória) disponíveis para
    esta seção e uma tamanho (opcional); e imprime o cabeçalho e a lista
    recebida como argumento no formato personalizado do programa; e inicia o
    loop principal desta seção para seleção e execução da opção escolhida
    pelo usuário.
    
    A função executar() recebe a ação escolhida pelo usuário (obrigatória) e
    um tamanho (opcional) como argumentos e utiliza uma estrutura match case
    para executar a função responsável por desempenhar a opção escolhida.
    Cada case foi temporariamente preechido com uma ellipsis, que
    futuramente será substituída pela chamada da devida função conforme
    estas forem implementadas.
    
    As funções cabecalho(), informacao(), menu_acoes() e selecionar_acao()
    foram importadas para auxiliar na implementação das funções editar() e
    executar().

--- editar_tarefas.py
+++ editar_tarefas.py
@@ -1,3 +1,34 @@
+from utilitarios import cabecalho, informacao, menu_acoes, selecionar_acao
+
+
+def editar(acoes: list[str], tamanho: int = 50) -> None:
+    cabecalho("editar tarefas")
+    menu_acoes(acoes, tamanho)
+
+    while True:
+        acao_escolhida: int = selecionar_acao(acoes, tamanho)
+        if acao_escolhida != len(acoes):
+            executar(acao_escolhida, tamanho)
+        else:
+            return
+
+
+def executar(acao_escolhida: int, tamanho: int = 50) -> None:
+    match acao_escolhida:
+        case 1:
+            ...
+        case 2:
+            ...
+        case 3:
+            ...
+        case 4:
+            ...
+        case 5:
+            ...
+        case _:
+            informacao("ação não implementada", tamanho)
+
+
 def main() -> None: ...
 
 
